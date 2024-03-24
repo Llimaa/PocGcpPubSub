d03ae06c0e5ae82d86bce5905eefe40c9b73e3bb
Marcos Limal <marcoslimadefatima@gmail.com>
2024-03-24 15:54:24 -0300

    feat: finished the subscriver service

---
+++ Subscriber/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using PopGCPPubSub.Subscriber;
+
+Console.WriteLine("Starting subscriber...");
+
+var projectId = "gcp-demo";
+var topicId = "test-pub";
+var subscriptionId = "test-sub";
+
+await SubscribeMessage.InitialConfigAsync(projectId, topicId, subscriptionId);
+
+await SubscribeMessage.PullMessagesAsync(projectId, subscriptionId);
---
+++ Subscriber/SubscribeMessage.cs
@@ -0,0 +1,41 @@
+
+using Google.Api.Gax;
+using Google.Cloud.PubSub.V1;
+
+namespace PopGCPPubSub.Subscriber;
+
+public sealed class SubscribeMessage 
+{
+    public static async Task PullMessagesAsync(string projectId, string subscriptionId, bool acknowledge = true)
+    {
+        var subscriptionName = new SubscriptionName(projectId, subscriptionId);
+
+        var subscriber = await new SubscriberClientBuilder 
+        {
+            SubscriptionName = subscriptionName,
+            EmulatorDetection = EmulatorDetection.EmulatorOrProduction
+        }.BuildAsync();
+
+        await subscriber.StartAsync((message, cancellationToken) =>
+        {
+            Console.WriteLine($"Received message with Id:  {message.MessageId}, published at {message.PublishTime.ToDateTime()}");
+            Console.WriteLine($"Message: '{message.Data.ToStringUtf8()}'");
+
+            return Task.FromResult(acknowledge ? SubscriberClient.Reply.Ack : SubscriberClient.Reply.Nack);
+        });
+    }
+
+    public static async Task InitialConfigAsync(string projectId, string topicId, string subscriptionId)
+    {
+        Environment.SetEnvironmentVariable("PUBSUB_EMULATOR_HOST", "localhost:8681");
+        var subscriptionName = new SubscriptionName(projectId, subscriptionId);
+        
+        var subscriberService = await new SubscriberServiceApiClientBuilder
+        {
+            EmulatorDetection = EmulatorDetection.EmulatorOrProduction
+        }.BuildAsync();
+
+        var topicName = new TopicName(projectId, topicId);
+        subscriberService.CreateSubscription(subscriptionName, topicName, pushConfig: null, ackDeadlineSeconds: 60);
+    }
+}
